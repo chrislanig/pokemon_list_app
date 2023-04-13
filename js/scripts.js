30a88dfc0df2fa346421ccc36b909666fe72f454
chrislanig <chrisandcourtneyfamilychannel@gmail.com>
2023-04-13 14:45:56 -0500

    Update scripts.js
    corrected file

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,16 @@
-let pokemonList = [];
-
-let pokemon1 = {
-    name: 'Bulbasaur',
-    height: 7,
-    types: ['grass', 'poison']
-};
-
-let pokemon2 = {
-    name: 'Charmander',
-    height: 6,
-    types: ['fire']
-};
-
-let pokemon3 = {
-    name: 'Squirtle',
-    height: 5,
-    types: ['water']
-}
+let pokemonList = [
+    {
+        name: 'Bulbasaur',
+        height: 7,
+        types: ['grass', 'poison'],
+    },
+    {   name: 'Charmander',
+        height: 6,
+        types: ['fire'],
+    },
+    {
+        name: 'Squirtle',
+        height: 5,
+        types: ['water'],
+    },
+];
