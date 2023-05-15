ffcc18ee8e9a101cf89e5791460730b5ba62ea5d
chrislanig <chrisandcourtneyfamilychannel@gmail.com>
2023-05-15 17:07:36 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 let pokemonList = [
-    {
-        name: 'Bulbasaur',
-        height: 7,
-        types: ['grass', 'poison'],
-    },
-    {   name: 'Charmander',
-        height: 6,
-        types: ['fire'],
-    },
-    {
-        name: 'Squirtle',
-        height: 5,
-        types: ['water'],
-    },
+{ name: 'Bulbasaur',
+height: 7,
+types: ['grass', 'poison'] },
+{ name: 'Charmander',
+height: 6,
+types: ['fire'] },
+
+{ name: 'Squirtle',
+height: 5,
+types: ['water'] },
 ];
+
+for (let i=0; i < pokemonList.length; i++) {
+    document.write (pokemonList[i]);
+}
