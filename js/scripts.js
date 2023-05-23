151a285e06d649e2fc6b6a246ca0d110c1deb7b0
chrislanig <chrisandcourtneyfamilychannel@gmail.com>
2023-05-23 14:38:45 -0500

    Update scripts.js
    added iife

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
+let pokemonRepository = (function () {
+    let pokemonList = [];
+  
+    function add(pokemon) {
+      pokemonList.push(pokemon);
+    }
+  
+    function getAll() {
+      return pokemonList;
+    }
+  
+    return {
+      add: add,
+      getAll: getAll
+    };
+  })();
+
 let pokemonList = [
 { name: 'Bulbasaur',
 height: 7,
